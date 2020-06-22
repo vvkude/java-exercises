0ac588f60c71266e3f95f1ac199e4c6fc0e00566
Vik Kudesia <vvkudesia@gmail.com>
2020-06-22 16:28:41 -0400

    UPDATE Resolve variable scoping error

--- basicjavaexercises/src/edu/vvkude/exercises/StudentVariables.java
+++ basicjavaexercises/src/edu/vvkude/exercises/StudentVariables.java
@@ -3,11 +3,12 @@ package edu.vvkude.exercises;
 public class StudentVariables {
 	private double marks1, marks2, marks3; // We do not want student marks accessed directly
 	private double maxMarks = 100; // Maximum scores for a class should not be directly mutable
+	private double avg = 0;
 	
 	public double getAverage() {
 		// We should consider replacing the denominator with the length of an array
 		// This way, we will not have to directly adjust the multiplier if there are more tests
-		double avg = 0;
+		
 		if (maxMarks > 0) {
 			avg = (marks1 + marks2 + marks3)/(maxMarks*3) * 100;
 			return avg;
@@ -17,10 +18,10 @@ public class StudentVariables {
 			return avg;
 		}
 	}
+		
+
 	
-	//TODO avg is local to the StudentVariables class and cannot be accessed outside of it
-	public void setAverage(double val) {
-		avg = val;
-	}
+	
+
 	
 }
