ce6e27ecf4ccfe03e5a4b887303f8b37a7c98643
Vik Kudesia <vvkudesia@gmail.com>
2020-06-22 09:22:44 -0400

    UPDATE Add getters and setters to the Employee class

--- basicjavaexercises/src/edu/vvkude/exercises/Employee.java
+++ basicjavaexercises/src/edu/vvkude/exercises/Employee.java
@@ -1,15 +1,28 @@
 package edu.vvkude.exercises;
 
 public class Employee {
-	String name;
-	int age;
+	private String name;
+	private int age;
+	
+	public void setName(String val) {
+		name = val;
+	}
+	
+	public void setAge(int years) {
+		age = years;
+	}
+	
 	public void printEmp() {
 		System.out.println("Name is " + name + " age is " + age);
 	}
 }
+
+// Directly setting object fields is bad practice. So, let's use getters and setters
 class Office {
 	public static void main(String[] args) {
 		Employee e1 = new Employee();
+		e1.setName("Bananas");
+		e1.setAge(27);
 		e1.printEmp();
 	}
 }
