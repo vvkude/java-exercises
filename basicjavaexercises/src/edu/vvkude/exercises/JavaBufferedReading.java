fb925e1d79a05aaadc6c7c2b159e7241f658a21e
Vik Kudesia <vvkudesia@gmail.com>
2020-07-23 15:02:00 -0400

    ADD Java Book exercises for input via Scanner and BufferedReader

---
+++ basicjavaexercises/src/edu/vvkude/exercises/JavaBufferedReading.java
@@ -0,0 +1,27 @@
+package edu.vvkude.exercises;
+// Intro to Java Book exercise - loading data from a csv
+// TODO - add io and nio imports
+
+
+public class JavaBufferedReading {
+
+	public static void main(String[] args) {
+		// TODO - Probably make an ArrayList to hold the information once it's read in from the csv
+		
+		// TODO - main method should consume the csv file and print out individual elements from it
+
+	}
+
+}
+
+// TODO - create a BufferedReader instance
+
+// TODO - implement the BufferedReader using a try-with-resources statement to ensure object closure (?)
+// => "any object that implements java.lang.AutoCloseable can be used as a resource"
+// => "all objects which implement java.io.Closeable implement java.lang.AutoCloseable"
+
+// TODO - use String.Split() to load a string array with the values from the csv
+
+// TODO - catch IOException
+
+// TODO - create a java class that will represent the data elements from our csv file and will implement getters and setters
